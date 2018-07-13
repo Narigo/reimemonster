fe3eb607e4f7d9eed9939823c09a99bbb3a9eaea
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-07-13 03:55:09 +0200

    Fix log output in service worker
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- docs/sw.js
+++ docs/sw.js
@@ -15,7 +15,7 @@ self.addEventListener("install", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
-  console.log("SW Request:", e.request.url);
+  console.log("SW Request:", event.request.url);
   event.respondWith(
     caches
       .match(event.request)
